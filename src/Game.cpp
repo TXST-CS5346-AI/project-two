3acb440e2802fc4da3e1cbbad334845dc9df575d
RRHenderson <78395772+RRHenderson@users.noreply.github.com>
2021-04-07 18:19:31 -0500

    Built member function headers in Game, Player

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,1 +1,48 @@
 #include "Game.hpp"
+
+
+Game::Game() // constructor
+{
+
+
+}
+Game::~Game()  // destructor
+{
+
+
+
+}
+
+// The only initialization function needed, as the game will 
+// be played automatically by 2 AI players (MIN and MAX). 
+// while gameOver == NOT_DONE keep playing
+
+void Game::startGame()
+{
+
+
+
+}
+
+// simple function to invert the enum value, thus determine who's turn is it next. 
+// E.g., if currentPlayer is RED (1), function returns BLACK (-1)
+
+Color Game::changePlayer(Color currentPlayer)
+{
+  
+
+    return Color::RED;
+
+}
+
+
+Game::GameOver Game::gameOver()
+{
+
+ 
+
+
+    return GameOver::DRAW;
+
+}
+
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,1 +1,47 @@
 #include "Player.hpp"
+
+Player::Player() // constructor
+{
+
+
+}
+Player::~Player() // destructor
+{
+
+
+}
+
+Player::Player(Color color) // overloaded constructor to set player color, which is IMMUTABLE
+{
+    this->color = color;
+
+}
+
+Board Player::takeTurn()
+{
+    Board board;
+
+
+    return board;
+
+}
+
+int Player::getNumPieces()
+{
+
+    return 0;
+}
+int Player::getNumPiecesTaken()
+{
+
+    return 0;
+}
+// player can not gain additional pieces during the game, only lose them
+void Player::decreasePlayerPieces(int piecesToSubtract)
+{
+
+
+
+}
+
+
