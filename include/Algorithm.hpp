41db7203518e49f906e8e25e91e11d98058571ab
bss8 <borislav.sabotinov@gmail.com>
2021-04-10 16:17:51 -0500

    Boris: Updating header and class for Algorithm.

--- include/Algorithm.hpp
+++ include/Algorithm.hpp
@@ -9,6 +9,7 @@
 
 /**
  * Header definition for class Algorithm. 
+ * @author multiple - David, Boris, and Randy
  * 
  * This class will encapsulate the algorithmic approach the AI uses to play Checkers. 
  * 
@@ -31,42 +32,42 @@ class Algorithm
 private: 
     struct Result {
         int value;
-        std::string path; 
+        Move move; 
     };
     int numNodesGenerated; 
     int evalVersion; 
-
-    // support functions for minimax
     
     // plausible move generator, returns a list of positions that can be made by player
-    std::vector<int> movegen(Board board, Player player);
+    std::vector<Move> movegen(Board board, Player player);
     
     /* static evaluation functions return a number representing the 
     * goodness of Position from the standpoint of Player
     * A helper function staticEval is used to determine which evalFunction to use
     */
+    Result evalFunctOne(Board position, Player p);
+    Result evalFunctTwo(Board position, Player p);
+    Result evalFunctThree(Board position, Player p);
 
-    int evalFunctOne(Board position, Player p);
-    int evalFunctTwo(Board position, Player p);
-    int evalFunctThree(Board position, Player p);
-
-    int staticEval(Board position, Player p, int evalVersion);
+    // wrapper function that will decide which of the actual three eval functions to call
+    Result staticEval(Board position, Player p, int evalVersion);
 
     // if true, return the structure
     bool deepEnough();
-    
+
+    bool terminalTest(Board state); // terminal test for alpha-beta-search
+    Result maxValue(Board state, int alpha, int beta); 
+    Result minValue(Board state, int alpha, int beta);
+
 public: 
     Algorithm(); // constructor
     ~Algorithm(); // destructor 
-
-    // main function for minimax alg
 
     // minimax algorithm returns the position of the best move
     Result minimax_a_b(Board board, int depth, Player p);
 
     // AB Prune algorithm
-    Result abPrune(Board state);
-    
+    Result alphaBetaSearch(Board state);
+
     void setEvalVersion(int evalVersion); 
 };
 
--- src/Algorithm.cpp
+++ src/Algorithm.cpp
@@ -1,2 +1,129 @@
 #include "Algorithm.hpp"
 
+#include <limits>
+
+
+Algorithm::Algorithm()
+{
+
+}
+
+Algorithm::~Algorithm()
+{
+
+}
+
+std::vector<Move> Algorithm::movegen(Board board, Player player)
+{
+    std::vector<Move> listOfPossibleMoves;
+    return listOfPossibleMoves;
+}
+
+Algorithm::Result Algorithm::evalFunctOne(Board position, Player p)
+{
+    Algorithm::Result result;
+    return result;
+}
+
+Algorithm::Result Algorithm::evalFunctTwo(Board position, Player p)
+{
+    Algorithm::Result result;
+    return result;
+}
+
+Algorithm::Result Algorithm::evalFunctThree(Board position, Player p)
+{
+    Algorithm::Result result;
+    return result;
+}
+
+// wrapper function that will decide which of the actual three eval functions to call
+Algorithm::Result Algorithm::staticEval(Board position, Player p, int evalVersion)
+{
+    Algorithm::Result result;
+    return result;
+}
+
+// minimax algorithm returns the position of the best move
+Algorithm::Result Algorithm::minimax_a_b(Board board, int depth, Player p)
+{
+    Algorithm::Result result;
+    return result;
+}
+
+/**
+ * Alpha Beta Search
+ * @author Borislav Sabotinov
+ */ 
+Algorithm::Result Algorithm::alphaBetaSearch(Board state)
+{
+    int alpha = std::numeric_limits<int>::min(); // tracks best value for max, initialized to WORST case
+    int beta = std::numeric_limits<int>::max(); // tracks best value for min, initialized to WORST case
+
+    Algorithm::Result result = maxValue(state, alpha, beta);
+
+    return result;
+}
+
+/**
+ * maxValue function determines 
+ * 
+ * @param Board state
+ * @param int alpha
+ * @param int beta
+ * 
+ * @return int utilityValue
+ */ 
+Algorithm::Result Algorithm::maxValue(Board state, int alpha, int beta)
+{
+    Algorithm::Result result;
+    
+    if (Algorithm::terminalTest(state))
+    {
+
+    }
+
+    return result;
+}
+
+/**
+ * minValue function determines
+ * 
+ * @param Board state
+ * @param int alpha
+ * @param int beta
+ * 
+ * @return  utilityValue
+ */  
+Algorithm::Result Algorithm::minValue(Board state, int alpha, int beta)
+{
+    Algorithm::Result result;
+    return result;
+}
+
+/**
+ * terminalTest function is used by alpha-beta-search to determine if 
+ * the move either ends the game or leads to a leaf node
+ * 
+ * First checks if we're at maxDepth (i.e. at a leaf/terminal node). 
+ * If yes, return true. Otherwise, next we check if the move ends the game. 
+ * For example - if Red player ends up with zero pieces, they lost and the game is over. 
+ * 
+ * @param Board state
+ * 
+ * @return bool isTerminalState
+ */ 
+bool Algorithm::terminalTest(Board state) 
+{
+    return false;
+}
+
+
+
+/**
+ * @author Borislav Sabotinov
+ */ 
+void Algorithm::setEvalVersion(int evalVersion)
+{
+    this->evalVersion = evalVersion;
+}
